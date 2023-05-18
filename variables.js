0edac0143c05bad8d5e53b1fd14be35ce199fe50
Moon ijaz <moonijaz006@gmail.com>
2023-05-18 09:50:00 +0500

    Add variables

---
+++ variables.js
@@ -0,0 +1,6 @@
+// number
+var userid = 2233;
+userid.toFixed();
+// userid = "bj" this will always give error bcz tsc is smart enough to know what to assign where
+// boolean
+var isloogedin = false;
