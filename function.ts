426c6b9ffd164721f5bb8f5f40b6ff5fc96adbaa
Moon ijaz <moonijaz006@gmail.com>
2023-05-21 08:40:00 +0500

    Add a funvtion which is giving any

---
+++ function.ts
@@ -0,0 +1,6 @@
+function addTwo(num : number) { // to solve erroe i have given num : number
+    return num // here i want to add 2 in a number
+}
+
+//addTwo("5") // but ive given a string which it shouldn't add and should give an error but its not and the type its giveng is any so to solve that
+addTwo(2)
